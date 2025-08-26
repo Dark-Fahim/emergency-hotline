5a7c12696b2a16b071d31eb5e4fcae9fd57f734f
Dark-Fahim <itsmunna1066@gmail.com>
2025-08-27 01:16:25 +0600

    heart count func added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log('object');
+// Heart count
+const heartCount = () => {
+    const heart = parseInt(document.getElementById('heart-count').innerText)
+    document.getElementById('heart-count').innerText = heart + 1
+}
+
+const callFunc = service => {
+    const credits = parseInt(document.getElementById('credits').innerText)
+    alert('Calling National emergency Service 999...')
+}
